5680173a2d4e11e9b84f59da6aa8e5d33072a337
giovannimalausa <83951798+giovannimalausa@users.noreply.github.com>
2022-01-23 12:23:18 +0100

    Update game.js

--- game.js
+++ game.js
@@ -2149,7 +2149,7 @@ function update () {
 
   // ===== CAMBIO LIVELLI =====
   // Cambio Livello 1 => 2
-  if (levelPlaying == 1 && player.x >= 12750) {
+  if (levelPlaying == 1 && player.x >= 12745) { // <== player.x >= 12750 precedente
     if (autoPilot == false) {
       autoPilot = true; // Fa sapere che il giocatore è controllato dal codice (disattiva il gameover)
       enableUserMovement = false; // Disabilita i controlli da parte dell'utente
